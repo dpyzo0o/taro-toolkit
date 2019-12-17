2f2532e75afa7ede9f06c917281b49239a740c18
dpyzo0o <yang.xu940121@outlook.com>
2019-12-17 15:34:58 +0800

    feat: update taro dependencies to 2.x

--- config/index.js
+++ config/index.js
@@ -25,27 +25,32 @@ const config = {
   },
   sourceRoot: 'src',
   outputRoot: 'dist',
-  plugins: {
-    babel: {
-      sourceMap: true,
-      presets: [
-        [
-          'env',
-          {
-            modules: false,
-          },
-        ],
+  babel: {
+    sourceMap: true,
+    presets: [
+      [
+        'env',
+        {
+          modules: false,
+        },
       ],
-      plugins: [
-        'transform-decorators-legacy',
-        'transform-class-properties',
-        'transform-object-rest-spread',
+    ],
+    plugins: [
+      'transform-decorators-legacy',
+      'transform-class-properties',
+      'transform-object-rest-spread',
+      [
+        'transform-runtime',
+        {
+          helpers: false,
+          polyfill: false,
+          regenerator: true,
+          moduleName: 'babel-runtime',
+        },
       ],
-    },
-    sass: {
-      importer: sassImporter,
-    },
+    ],
   },
+  sass: {},
   defineConstants: {},
   alias: {
     '~': path.resolve(__dirname, '..', 'src'),
@@ -54,52 +59,51 @@ const config = {
     patterns: [],
     options: {},
   },
-  weapp: {
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
-          },
-        },
-        pxtransform: {
-          enable: true,
-          config: {},
-        },
-        url: {
-          enable: true,
-          config: {
-            limit: 10240, // 设定转换尺寸上限
-          },
-        },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]',
-          },
+  mini: {
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+          browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
         },
       },
+      pxtransform: {
+        enable: true,
+        config: {},
+      },
+      url: {
+        enable: true,
+        config: {
+          limit: 10240, // 设定转换尺寸上限
+        },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
+        },
+      },
+    },
+    sassLoaderOption: {
+      importer: sassImporter,
     },
   },
   h5: {
     publicPath: '/',
     staticDirectory: 'static',
-    module: {
-      postcss: {
-        autoprefixer: {
-          enable: true,
-          config: {
-            browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
-          },
+    postcss: {
+      autoprefixer: {
+        enable: true,
+        config: {
+          browsers: ['last 3 versions', 'Android >= 4.1', 'ios >= 8'],
         },
-        cssModules: {
-          enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
-          config: {
-            namingPattern: 'module', // 转换模式，取值为 global/module
-            generateScopedName: '[name]__[local]___[hash:base64:5]',
-          },
+      },
+      cssModules: {
+        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        config: {
+          namingPattern: 'module', // 转换模式，取值为 global/module
+          generateScopedName: '[name]__[local]___[hash:base64:5]',
         },
       },
     },
