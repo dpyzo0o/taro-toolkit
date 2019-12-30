fa87ccd53caf39ac2aecb6699282b93c81960915
dpyzo0o <yang.xu940121@outlook.com>
2019-12-30 14:42:40 +0800

    chore: disable realtime logger in dev

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,22 +1,23 @@
 import Taro from '@tarojs/taro';
 
 const log = Taro.getRealtimeLogManager();
+const isDev = process.env.NODE_ENV === 'development';
 
 export default {
   info(...args: any[]) {
-    if (!log) {
+    if (!log || isDev) {
       return;
     }
     log.info(...args);
   },
   warn(...args: any[]) {
-    if (!log) {
+    if (!log || isDev) {
       return;
     }
     log.warn(...args);
   },
   error(...args: any[]) {
-    if (!log) {
+    if (!log || isDev) {
       return;
     }
     log.error(...args);
@@ -27,7 +28,7 @@ export default {
    * @since 2.7.3
    */
   setFilterMsg(msg: string) {
-    if (!log || !log.setFilterMsg) {
+    if (!log || !log.setFilterMsg || isDev) {
       return;
     }
     log.setFilterMsg(msg);
@@ -38,7 +39,7 @@ export default {
    * @since 2.8.1
    */
   addFilterMsg(msg: string) {
-    if (!log || !log.addFilterMsg) {
+    if (!log || !log.addFilterMsg || isDev) {
       return;
     }
     log.addFilterMsg(msg);
