cfc902e5bd5240393e885726a07b4d92d933c1b1
dpyzo0o <yang.xu940121@outlook.com>
2020-07-02 11:27:46 +0800

    fix: missing react-dom

--- src/app.tsx
+++ src/app.tsx
@@ -13,28 +13,30 @@ import './app.scss';
 
 class App extends React.Component {
   componentDidMount() {
-    const updateManager = Taro.getUpdateManager();
+    if (process.env.TARO_ENV === 'weapp') {
+      const updateManager = Taro.getUpdateManager();
 
-    updateManager.onCheckForUpdate(res => {
-      console.log(res);
-    });
-
-    updateManager.onUpdateReady(() => {
-      Taro.showModal({
-        title: '更新提示',
-        content: '新版本已经准备好，是否重启应用？',
-        success: function(res) {
-          if (res.confirm) {
-            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
-            updateManager.applyUpdate();
-          }
-        },
+      updateManager.onCheckForUpdate(res => {
+        console.log(res);
       });
-    });
 
-    updateManager.onUpdateFailed(res => {
-      console.log(res);
-    });
+      updateManager.onUpdateReady(() => {
+        Taro.showModal({
+          title: '更新提示',
+          content: '新版本已经准备好，是否重启应用？',
+          success: function (res) {
+            if (res.confirm) {
+              // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+              updateManager.applyUpdate();
+            }
+          },
+        });
+      });
+
+      updateManager.onUpdateFailed(res => {
+        console.log(res);
+      });
+    }
   }
 
   componentDidShow() {}
